f81ea9634652dde0a71f932d8cc08434ddbb5a1f
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2020-12-24 02:26:21 +0100

    Use arrays instead of lists

--- src/main/kotlin/day22/crabCombatPart2.kt
+++ src/main/kotlin/day22/crabCombatPart2.kt
@@ -3,23 +3,27 @@ package day22.part2
 import day22.part2.GameResult.A_WINS
 import day22.part2.GameResult.B_WINS
 import util.readDayInput
+import util.readSampleInput
 import util.splitByEmptyLines
 
+val N = 50
+
 fun main() {
+    val input = readSampleInput("day22").splitByEmptyLines()
     val (playerA, playerB) =
 //      readSampleInput("day22", "sampleInputPart2")
-        readDayInput("day22")
-            .splitByEmptyLines()
-            .map { Player(it.subList(1, it.size).map(String::toInt)) }
+        input.map { list ->
+            Player(list.subList(1, list.size).map(String::toInt))
+        }
     val game = playGame(playerA, playerB, GameIndex.nextIndex())
     val finalConfiguration = game.finalConfiguration
     log("\n\n== Post-game results ==")
-    finalConfiguration.logDecks()
-    println(finalConfiguration.calculateScore())
+    finalConfiguration?.logDecks()
+    println(finalConfiguration?.calculateScore())
 }
 
 enum class GameResult(val winner: String) { A_WINS("1"), B_WINS("2") }
-data class Result(val gameResult: GameResult, val finalConfiguration: RoundConfiguration)
+data class Result(val gameResult: GameResult, val finalConfiguration: RoundConfiguration?)
 
 object PlayedGames {
     private val games = mutableMapOf<RoundConfiguration, Result>()
@@ -33,9 +37,9 @@ object PlayedGames {
 }
 
 fun playGame(playerA: Player, playerB: Player, gameIndex: Int): Result {
-    println("=== Game $gameIndex ===")
+    log("=== Game $gameIndex ===")
     val configuration = RoundConfiguration(playerA, playerB)
-    println(configuration)
+    log(configuration)
     val oldResult = PlayedGames.checkGame(configuration)
     if (oldResult != null) return oldResult.also { println("Repetition!") }
     val round = Round(configuration, setOf(), 1, gameIndex)
@@ -43,19 +47,56 @@ fun playGame(playerA: Player, playerB: Player, gameIndex: Int): Result {
     val resultingRound = rounds.last()
     val gameResult = when {
         resultingRound.repeatsItself() -> A_WINS
-        resultingRound.playerA.cards.size > resultingRound.playerA.cards.size -> A_WINS
+        resultingRound.playerA.size > resultingRound.playerA.size -> A_WINS
         else -> B_WINS
     }
-    println("The winner of game $gameIndex is player ${gameResult.winner}!")
-    return Result(gameResult, rounds.last().configuration)
-        .also { PlayedGames.recordGame(configuration, it) }
+    log("The winner of game $gameIndex is player ${gameResult.winner}!")
+    return Result(gameResult, resultingRound.configuration)
+        .also { PlayedGames.recordGame(configuration, Result(gameResult, null)) }
 }
 
-fun List<Int>.tail() = subList(1, size)
+fun IntArray.tail() = IntArray(N) { this.getOrElse(it + 1) { 0 } }
 
-data class Player(val cards: List<Int>) {
-    fun losing() = Player(cards.tail())
-    fun winning(winningCards: List<Int>) = Player(cards.tail() + winningCards)
+data class Player(
+    val cards: IntArray,
+    val size: Int,
+) {
+    constructor(list: List<Int>): this(IntArray(N), list.size) {
+        for ((index, element) in list.withIndex()) {
+            cards[index] = element
+        }
+    }
+    fun isEmpty() = size == 0
+    fun nextRound(number: Int): Player {
+        val new = IntArray(N)
+        cards.copyInto(new, startIndex = 1, endIndex = number + 1)
+        return Player(new, number)
+    }
+    fun losing() = Player(cards.tail(), size - 1)
+    fun winning(first: Int, second: Int): Player {
+        val tail = cards.tail()
+        tail[size - 1] = first
+        tail[size] = second
+        return Player(tail, size + 1)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Player
+
+        if (!cards.contentEquals(other.cards)) return false
+        if (size != other.size) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = cards.contentHashCode()
+        result = 31 * result + size
+        return result
+    }
 }
 
 data class Round(
@@ -75,41 +116,40 @@ data class RoundConfiguration(
 )
 
 fun RoundConfiguration.calculateScore(): Long {
-    val winner = listOf(playerA, playerB).maxByOrNull { it.cards.size }!!
-    val size = winner.cards.size
+    val winner = listOf(playerA, playerB).maxByOrNull { it.size }!!
+    val size = winner.size
     return winner.cards
         .foldIndexed(0L) { index, acc, element -> acc + (size - index) * element }
 }
 
 fun Round.nextRound(): Round? {
     if (repeatsItself()) return null
-    if (playerA.cards.isEmpty() || playerB.cards.isEmpty()) return null
+    if (playerA.isEmpty() || playerB.isEmpty()) return null
     val cardA = playerA.cards.first()
     val cardB = playerB.cards.first()
 
     logRoundStart(cardA, cardB)
 
-    fun Player.hasEnoughCards() = cards.size - 1 >= cards.first()
+    fun Player.hasEnoughCards() = size - 1 >= cards.first()
     if (!playerA.hasEnoughCards() || !playerB.hasEnoughCards()) {
-        val winningCards = listOf(cardA, cardB).sortedDescending()
         return if (cardA >= cardB) {
-            createNextRound(playerA.winning(winningCards), playerB.losing(), A_WINS)
+            createNextRound(playerA.winning(cardA, cardB), playerB.losing(), A_WINS)
         } else {
-            createNextRound(playerA.losing(), playerB.winning(winningCards), B_WINS)
+            createNextRound(playerA.losing(), playerB.winning(cardB, cardA), B_WINS)
         }
     }
 
     log("Playing a sub-game to determine the winner...\n")
     val game = playGame(
-        Player(playerA.cards.tail().take(cardA)),
-        Player(playerB.cards.tail().take(cardB)),
+        playerA.nextRound(cardA),
+        playerB.nextRound(cardB),
         GameIndex.nextIndex()
     )
     log("\n...anyway, back to game $gameIndex.")
     return if (game.gameResult == A_WINS) {
-        createNextRound(playerA.winning(listOf(cardA, cardB)), playerB.losing(), A_WINS)
+        createNextRound(playerA.winning(cardA, cardB), playerB.losing(), A_WINS)
     } else {
-        createNextRound(playerA.losing(), playerB.winning(listOf(cardB, cardA)), B_WINS)
+        createNextRound(playerA.losing(), playerB.winning(cardB, cardA), B_WINS)
     }
 }
 
@@ -128,9 +168,9 @@ fun Round.logRoundStart(cardA: Int, cardB: Int) {
 }
 
 fun RoundConfiguration.logDecks() {
-    fun Player.logCards() = cards.joinToString()
-    log("Player 1's deck: ${playerA.logCards()}")
-    log("Player 2's deck: ${playerB.logCards()}")
+    fun Player.logCards() = cards.filter { it != 0 }.joinToString()
+    log("Player 1's deck: ${playerA.logCards()} size = ${playerA.size}")
+    log("Player 2's deck: ${playerB.logCards()} size = ${playerB.size}")
 }
 
 fun Round.logRoundResult(gameResult: GameResult) {
