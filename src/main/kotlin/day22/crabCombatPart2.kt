07f812c17b361528a4528bffe0d6b0e29bd66ae6
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2020-12-24 10:27:16 +0100

    Separated two classes: player with array & with list

--- src/main/kotlin/day22/crabCombatPart2.kt
+++ src/main/kotlin/day22/crabCombatPart2.kt
@@ -6,19 +6,19 @@ import util.readDayInput
 import util.readSampleInput
 import util.splitByEmptyLines
 
-val N = 50
+val DEBUG = false
 
 fun main() {
     val input = readDayInput("day22").splitByEmptyLines()
     val (playerA, playerB) =
 //      readSampleInput("day22", "sampleInputPart2")
         input.map { list ->
-            Player(list.subList(1, list.size).map(String::toInt))
+            PlayerOptimized(list.subList(1, list.size).map(String::toInt))
         }
     val game = playGame(RoundConfiguration(playerA, playerB), GameIndex.nextIndex())
     val finalConfiguration = game.finalConfiguration
     log("\n\n== Post-game results ==")
-    finalConfiguration?.logDecks()
+    finalConfiguration?.logDecks(true)
     println(finalConfiguration?.calculateScore())
 }
 
@@ -53,40 +53,94 @@ fun playGame(configuration: RoundConfiguration, gameIndex: Int): Result {
         .also { PlayedGames.recordGame(configuration, gameResult) }
 }
 
-fun IntArray.tail() = IntArray(N) { this.getOrElse(it + 1) { 0 } }
+fun List<Int>.tail() = subList(1, size)
 
-data class Player(
-    val cards: IntArray,
-    val size: Int,
-) {
-    constructor(list: List<Int>): this(IntArray(N), list.size) {
-        for ((index, element) in list.withIndex()) {
-            cards[index] = element
-        }
+interface Player {
+    val size: Int
+    val first: Int
+    operator fun get(i: Int): Int
+    fun losing(): Player
+    fun winning(card1: Int, card2: Int): Player
+    fun isEmpty(): Boolean
+    fun hasEnoughCards() = size - 1 >= first
+    fun nextGamePlayer(size: Int, replacements: Map<Int, Int>): Player
+    fun cards(): List<Int>
+}
+
+data class PlayerCompact(val cards: List<Int>) : Player {
+    override val first get() = cards.first()
+    override val size get() = cards.size
+    override fun get(i: Int): Int = cards[i]
+
+    override fun losing() = PlayerCompact(cards.tail())
+    override fun winning(card1: Int, card2: Int) = PlayerCompact(cards.tail() + card1 + card2)
+    override fun isEmpty() = cards.isEmpty()
+
+    override fun nextGamePlayer(
+        size: Int,
+        replacements: Map<Int, Int>
+    ) = PlayerCompact(
+        cards
+            .subList(1, 1 + size)
+            .map { replacements.getValue(it) }
+    )
+
+    override fun cards(): List<Int> = cards
+}
+
+data class PlayerOptimized(
+    val cardsArray: IntArray
+) : Player {
+    constructor(list: List<Int>) : this(list.toIntArray())
+
+    override val size
+        get() = cardsArray.size
+
+    override val first: Int
+        get() = cardsArray.first()
+
+    override fun get(i: Int): Int = cardsArray[i]
+
+    override fun isEmpty() = size == 0
+
+    private fun IntArray.tail(size: Int) = IntArray(size) { this.getOrElse(it + 1) { 0 } }
+    override fun losing() = PlayerOptimized(cardsArray.tail(size - 1))
+
+    override fun winning(card1: Int, card2: Int): PlayerOptimized {
+        val tail = cardsArray.tail(size + 1)
+        tail[size - 1] = card1
+        tail[size] = card2
+        return PlayerOptimized(tail)
     }
-    fun isEmpty() = size == 0
-    fun losing() = Player(cards.tail(), size - 1)
-    fun winning(first: Int, second: Int): Player {
-        val tail = cards.tail()
-        tail[size - 1] = first
-        tail[size] = second
-        return Player(tail, size + 1)
+
+    override fun cards(): List<Int> = cardsArray.toList()
+
+    override fun nextGamePlayer(
+        size: Int,
+        replacements: Map<Int, Int>
+    ): PlayerOptimized {
+        val new = IntArray(size)
+        cardsArray.copyInto(new, startIndex = 1, endIndex = size + 1)
+        for (i in 0 until size) {
+            new[i] = replacements.getValue(new[i])
+        }
+        return PlayerOptimized(new)
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as Player
+        other as PlayerOptimized
 
-        if (!cards.contentEquals(other.cards)) return false
+        if (!cardsArray.contentEquals(other.cardsArray)) return false
         if (size != other.size) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = cards.contentHashCode()
+        var result = cardsArray.contentHashCode()
         result = 31 * result + size
         return result
     }
@@ -107,35 +161,22 @@ data class RoundConfiguration(
     val playerA: Player,
     val playerB: Player,
 ) {
-    fun nextGamePlayer(
-        player: Player,
-        size: Int,
-        replacements: Map<Int, Int>
-    ): Player {
-        val new = IntArray(N)
-        player.cards.copyInto(new, startIndex = 1, endIndex = size + 1)
-        for (i in 0 until size) {
-            new[i] = replacements.getValue(new[i])
-        }
-        return Player(new, size)
-    }
-
     fun nextGameConfiguration(): RoundConfiguration {
-        val cardA = playerA.cards.first()
-        val cardB = playerB.cards.first()
+        val cardA = playerA.first
+        val cardB = playerB.first
         val values = mutableListOf<Int>()
         for (i in 1..cardA) {
-            values += playerA.cards[i]
+            values += playerA[i]
         }
         for (j in 1..cardB) {
-            values += playerB.cards[j]
+            values += playerB[j]
         }
         values.sort()
         val replacements =
             values.withIndex().associate { it.value to it.index + 1 }
 
-        val nextA = nextGamePlayer(playerA, cardA, replacements)
-        val nextB = nextGamePlayer(playerB, cardB, replacements)
+        val nextA = playerA.nextGamePlayer(cardA, replacements)
+        val nextB = playerB.nextGamePlayer(cardB, replacements)
         return RoundConfiguration(nextA, nextB)
     }
 }
@@ -143,19 +184,18 @@ data class RoundConfiguration(
 fun RoundConfiguration.calculateScore(): Long {
     val winner = listOf(playerA, playerB).maxByOrNull { it.size }!!
     val size = winner.size
-    return winner.cards
+    return winner.cards()
         .foldIndexed(0L) { index, acc, element -> acc + (size - index) * element }
 }
 
 tailrec fun playNextRound(round: Round): Round {
     if (round.repeatsItself()) return round
     if (round.playerA.isEmpty() || round.playerB.isEmpty()) return round
-    val cardA = round.playerA.cards.first()
-    val cardB = round.playerB.cards.first()
+    val cardA = round.playerA.first
+    val cardB = round.playerB.first
 
     round.logRoundStart(cardA, cardB)
 
-    fun Player.hasEnoughCards() = size - 1 >= cards.first()
     val nextRound = if (!round.playerA.hasEnoughCards() || !round.playerB.hasEnoughCards()) {
         if (cardA >= cardB) {
             round.createNextRound(round.playerA.winning(cardA, cardB), round.playerB.losing(), A_WINS)
@@ -169,7 +209,7 @@ tailrec fun playNextRound(round: Round): Round {
             GameIndex.nextIndex()
         )
         log("\n...anyway, back to game ${round.gameIndex}.")
-        return if (game.gameResult == A_WINS) {
+        if (game.gameResult == A_WINS) {
             round.createNextRound(round.playerA.winning(cardA, cardB), round.playerB.losing(), A_WINS)
         } else {
             round.createNextRound(round.playerA.losing(), round.playerB.winning(cardB, cardA), B_WINS)
@@ -192,10 +232,10 @@ fun Round.logRoundStart(cardA: Int, cardB: Int) {
     log("Player 2 plays: $cardB")
 }
 
-fun RoundConfiguration.logDecks() {
-    fun Player.logCards() = cards.filter { it != 0 }.joinToString()
-    log("Player 1's deck: ${playerA.logCards()} size = ${playerA.size}")
-    log("Player 2's deck: ${playerB.logCards()} size = ${playerB.size}")
+fun RoundConfiguration.logDecks(debug: Boolean = DEBUG) {
+    fun Player.logCards() = cards().filter { it != 0 }.joinToString()
+    log("Player 1's deck: ${playerA.logCards()}", debug)
+    log("Player 2's deck: ${playerB.logCards()}", debug)
 }
 
 fun Round.logRoundResult(gameResult: GameResult) {
@@ -207,7 +247,6 @@ object GameIndex {
     fun nextIndex(): Int = maxIndex++
 }
 
-val debug = false
-fun log(message: Any? = "") {
+fun log(message: Any? = "", debug: Boolean = DEBUG) {
     if (debug) println(message)
 }
